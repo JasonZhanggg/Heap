b7733666ae0b048beae2e02f31433137f7cb9e4d
U-BSD\383349 <383349@SHS-10DEC4E04.beaverton.k12.or.us>
2020-02-24 10:48:51 -0800

    first commit

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void heapify (int[], int, int);
+
+void heapify(int arr[], int in){
+	int largest;
+	int l = 2*i+1;
+	int r = 2*i+2;
+	if(l<in && arr[1] >arr[largest]);
+		largest = l;
+	if
+
+
+}
+int main(){
+
+
+}
